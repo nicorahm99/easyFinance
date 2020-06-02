9021f40c65c21db0514d041e4ba6d6a396fbb5b4
lange98 <lange.dominik@student.dhbw-karlsuhe.de>
2020-06-02 10:51:11 +0200

    added 3 setting-option buttons

--- src/lib/settings/settings.dart
+++ src/lib/settings/settings.dart
@@ -1,3 +1,4 @@
+import 'package:charts_flutter/flutter.dart' as prefix0;
 import 'package:flutter/material.dart';
 import 'container.dart';
 
@@ -18,12 +19,12 @@ class Settings extends StatelessWidget {
       //     Div(Colors.red)
       //   ]
       // )
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
         elevation: 0,
         brightness: Brightness.light,
         iconTheme: IconThemeData(color: Colors.black),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey.shade200,
         title: Text('Settings', style: TextStyle(color: Colors.black),),
       ),
       body: SingleChildScrollView(
@@ -31,17 +32,68 @@ class Settings extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start, 
           children: <Widget>[
             Card(
+              elevation: 4.0, // shadow
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
               margin: const EdgeInsets.all(8.0),
               color: Colors.green,
               child: ListTile(
+                onTap: (){
+                  //open edit profile
+                },
                 title: Text('TestName', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),),
                 leading: CircleAvatar(backgroundColor: Colors.lightGreen[200]),
                 trailing: Icon(Icons.edit, color: Colors.white,),
               ),
+            ),
+            const SizedBox(height: 10.0),
+            Card(
+              elevation: 4.0, // shadow
+              margin: const EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 16.0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.lock_outline, color: Colors.green,),
+                    title: Text('Change Password'),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: (){
+                      //open change Password
+                    },
+                  ),
+                  buildDevider(),
+                  ListTile(
+                    leading: Icon(Icons.language, color: Colors.green,),
+                    title: Text('Change Language'),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: (){
+                      //open change language
+                    },
+                  ),
+                  buildDevider(),
+                  ListTile(
+                    leading: Icon(Icons.location_on, color: Colors.green,),
+                    title: Text('Change Location'),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: (){
+                      //open change location
+                    },
+                  ),
+                ],
+              ),
             )
+
           ],
         )
       ),
     );
+  }
+
+  Container buildDevider() {
+    return Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 8.0,),
+                  width: double.infinity,
+                  height: 1.0,
+                  color: Colors.grey.shade400,
+                );
   }
 }
