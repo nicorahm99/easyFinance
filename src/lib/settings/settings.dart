0f1fcabed6b8b4d3ff35b122b81ae46c13a1ba72
lange98 <lange.dominik@student.dhbw-karlsuhe.de>
2020-06-02 10:04:53 +0200

    built settings base-scaffold

--- src/lib/settings/settings.dart
+++ src/lib/settings/settings.dart
@@ -8,16 +8,34 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Div(Colors.red),
-          Div(Colors.green),
-          Div(Colors.blue),
-          Div(Colors.red)
-        ]
-      )
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //   crossAxisAlignment: CrossAxisAlignment.stretch,
+      //   children: [
+      //     Div(Colors.red),
+      //     Div(Colors.green),
+      //     Div(Colors.blue),
+      //     Div(Colors.red)
+      //   ]
+      // )
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('Settings'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, 
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                title: Text('TestName'),
+                leading: CircleAvatar(backgroundColor: Colors.green),
+                trailing: Icon(Icons.edit),
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
